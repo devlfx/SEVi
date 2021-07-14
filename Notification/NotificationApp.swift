8aa54776d23391c96e0827ed08c7f73abc1961d7
abe <laog18@gmail.com>
2021-07-14 17:30:40 -0500

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Luis Abraham Ortega Gonzalez on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
